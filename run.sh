ed660a22fdd5bbd9e48f4cf08711e85c4c5d613e
AtlasGao <ygao45@uthsc.edu>
2020-08-17 10:55:52 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+python ./examples/BRCA/BRCA-OS-4.py
+python ./examples/GBMLGG/GBMLGG_DSS.py
+python ./examples/GBMLGG/GBMLGG_OS.py
+python ./examples/KIPAN/KIPAN-DSS.py
+python ./examples/KIPAN/KIPAN-PFI.py
+python ./examples/KIPAN/KIPAN-OS.py
+
+python ./examples/PanGyn/PanGyn-DFI.py
+python ./examples/PRAD/PRAD_PFI.py
+python ./examples/STAD/STAD-PFI.py
+python ./examples/MMRF-OS.py
+
